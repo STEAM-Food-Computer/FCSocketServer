ed84f98936a8e7e560ea006cc12f204237f79535
Jackie ngo <jackie.ngo888@gmail.com>
2018-02-04 17:52:23 -0500

    added comments

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
-
 net = require('net');
+
 net.createServer(function (socket) {
 
-  // Identify this client
-  socket.name = socket.remoteAddress + ":" + socket.remotePort;
-  console.log("name:"  + socket.name);
-  // Send a nice welcome message and announce
-  socket.write("Welcome " + socket.name + "\n");
+    // Identify this client
+    socket.name = socket.remoteAddress + ":" + socket.remotePort;
+    console.log("name:"  + socket.name);
+    // Send message to client
+    socket.write("Welcome " + socket.name + "\n");
 
 
-  // Handle incoming messages from clients.
-  socket.on('data', function (data) {
-       console.log(stringFromUTF8Array(data));
-  });
+    // Handle incoming messages from clients. Convert byte array to string UTF-8 format
+    socket.on('data', function (data) {
+        console.log(stringFromUTF8Array(data));
+    });
 
-  // Remove the client from the list when it leaves
-  socket.on('end', function () {
-      console.log("client ended");
-  });
+    // display message when client leaves
+    socket.on('end', function () {
+        console.log("client ended");
+    });
 
-  function stringFromUTF8Array(data)
-{
-  const extraByteMap = [ 1, 1, 1, 1, 2, 2, 3, 0 ];
-  var count = data.length;
-  var str = "";
-
-  for (var index = 0;index < count;)
-  {
-    var ch = data[index++];
-    if (ch & 0x80)
+    function stringFromUTF8Array(data)
     {
-      var extra = extraByteMap[(ch >> 3) & 0x07];
-      if (!(ch & 0x40) || !extra || ((index + extra) > count))
-        return null;
+        const extraByteMap = [ 1, 1, 1, 1, 2, 2, 3, 0 ];
+        var count = data.length;
+        var str = "";
 
-      ch = ch & (0x3F >> extra);
-      for (;extra > 0;extra -= 1)
-      {
-        var chx = data[index++];
-        if ((chx & 0xC0) != 0x80)
-          return null;
+        for (var index = 0;index < count;)
+        {
+            var ch = data[index++];
+            if (ch & 0x80)
+            {
+                var extra = extraByteMap[(ch >> 3) & 0x07];
+                if (!(ch & 0x40) || !extra || ((index + extra) > count))
+                return null;
 
-        ch = (ch << 6) | (chx & 0x3F);
-      }
+                ch = ch & (0x3F >> extra);
+                for (;extra > 0;extra -= 1)
+                {
+                    var chx = data[index++];
+                    if ((chx & 0xC0) != 0x80)
+                    return null;
+
+                    ch = (ch << 6) | (chx & 0x3F);
+                }
+            }
+            str += String.fromCharCode(ch);
+        }
+        return str;
     }
-    str += String.fromCharCode(ch);
-  }
-  return str;
-}
 
 }).listen(5000);
 
-// Put a friendly message on the terminal of the server.
-console.log("Server running on port 5000\n");
+console.log("Server running on port 5000");
